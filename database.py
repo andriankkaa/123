ea8bf05e95dab24fd0810b24abf89ac507f32528
andriankkaa <134884081+andriankkaa@users.noreply.github.com>
2023-11-13 20:32:23 +0200

    Add files via upload

---
+++ database.py
@@ -0,0 +1,69 @@
+<<<<<<< HEAD
+# Модуль для роботи з конфігураційними файлами
+import configparser
+
+# Функція для зчитування налаштувань з файлу
+def read_settings():
+    config = configparser.ConfigParser()
+    try:
+        config.read('config.ini')  # Назва файлу конфігурації
+        settings = {
+            'max_attempts': int(config.get('GameSettings', 'max_attempts', fallback=5))  # Зчитуємо максимальну кількість спроб
+        }
+    except (configparser.NoSectionError, configparser.NoOptionError, ValueError):
+        settings = {'max_attempts': 5}  # Значення за замовчуванням
+    return settings
+
+# Функція для запису налаштувань у файл
+def write_settings(settings):
+    config = configparser.ConfigParser()
+    config['GameSettings'] = {
+        'max_attempts': str(settings['max_attempts'])
+    }
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+
+# Викликаємо функції для зчитування та запису налаштувань
+if __name__ == "__main__":
+    settings = read_settings()
+    print(f"Поточні налаштування: {settings}")
+
+    # Можна змінити налаштування
+    settings['max_attempts'] = 10
+    write_settings(settings)
+    print(f"Нові налаштування: {settings}")
+=======
+# Модуль для роботи з конфігураційними файлами
+import configparser
+
+# Функція для зчитування налаштувань з файлу
+def read_settings():
+    config = configparser.ConfigParser()
+    try:
+        config.read('config.ini')  # Назва файлу конфігурації
+        settings = {
+            'max_attempts': int(config.get('GameSettings', 'max_attempts', fallback=5))  # Зчитуємо максимальну кількість спроб
+        }
+    except (configparser.NoSectionError, configparser.NoOptionError, ValueError):
+        settings = {'max_attempts': 5}  # Значення за замовчуванням
+    return settings
+
+# Функція для запису налаштувань у файл
+def write_settings(settings):
+    config = configparser.ConfigParser()
+    config['GameSettings'] = {
+        'max_attempts': str(settings['max_attempts'])
+    }
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+
+# Викликаємо функції для зчитування та запису налаштувань
+if __name__ == "__main__":
+    settings = read_settings()
+    print(f"Поточні налаштування: {settings}")
+
+    # Можна змінити налаштування
+    settings['max_attempts'] = 10
+    write_settings(settings)
+    print(f"Нові налаштування: {settings}")
+>>>>>>> origin/main
---
+++ main.py
@@ -0,0 +1,71 @@
+<<<<<<< HEAD
+import PySimpleGUI as sg
+from game_logic import choose_random_word, guess_word
+
+# Додамо можливість гравцю вводити слово для вгадування
+def custom_guess():
+    layout = [
+        [sg.Text("Введіть слово для вгадування: "), sg.InputText(key='input_word')],
+        [sg.Button("Вгадати"), sg.Button("Вийти")]
+    ]
+
+    window = sg.Window("Спробуйте вгадати слово", layout)
+
+    while True:
+        event, values = window.read()
+
+        if event in (None, 'Вийти'):
+            break
+        if event == 'Вгадати':
+            guessed_word = values['input_word'].strip()
+            result = "Правильно!" if guessed_word == target_word else "Спробуйте ще раз."
+            sg.popup(result)
+
+    window.close()
+
+if __name__ == "__main__":
+    sg.theme("LightGreen4")
+
+    print("Гра 'Вгадайте слово!'")
+    print("Фрукти, які ви можете вибрати: яблуко, абрикос, апельсин, банан, персик, лимон, слива, груша, полуниця, ківі")
+
+    target_word = choose_random_word()
+
+    # Викличемо нову функцію для вгадування слів
+    custom_guess()
+=======
+import PySimpleGUI as sg
+from game_logic import choose_random_word, guess_word
+
+# Додамо можливість гравцю вводити слово для вгадування
+def custom_guess():
+    layout = [
+        [sg.Text("Введіть слово для вгадування: "), sg.InputText(key='input_word')],
+        [sg.Button("Вгадати"), sg.Button("Вийти")]
+    ]
+
+    window = sg.Window("Спробуйте вгадати слово", layout)
+
+    while True:
+        event, values = window.read()
+
+        if event in (None, 'Вийти'):
+            break
+        if event == 'Вгадати':
+            guessed_word = values['input_word'].strip()
+            result = "Правильно!" if guessed_word == target_word else "Спробуйте ще раз."
+            sg.popup(result)
+
+    window.close()
+
+if __name__ == "__main__":
+    sg.theme("LightGreen4")
+
+    print("Гра 'Вгадайте слово!'")
+    print("Фрукти, які ви можете вибрати: яблуко, абрикос, апельсин, банан, персик, лимон, слива, груша, полуниця, ківі")
+
+    target_word = choose_random_word()
+
+    # Викличемо нову функцію для вгадування слів
+    custom_guess()
+>>>>>>> origin/main
